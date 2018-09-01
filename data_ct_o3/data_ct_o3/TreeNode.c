d3d7d3a2da734f5fafc7ba574a99e9610fffcd60
Eugene Sergienko <Sergienko4@gmail.com>
2018-09-01 18:44:10 +0300

    added swich

--- data_ct_o3/data_ct_o3/TreeNode.c
+++ data_ct_o3/data_ct_o3/TreeNode.c
@@ -23,6 +23,7 @@ TreeNode * createTreeNode(type_data data)
 	TreeNode * temp = (TreeNode*)malloc(sizeof(TreeNode));
 	temp->data = data;
 	temp->h = 0;
+	temp->parent = NULL;
 	temp->left = temp->right = NULL;
 	return temp;
 }
@@ -34,23 +35,26 @@ TreeNode * insertValueToBST(TreeNode *root, type_data data)
 	int right = 0, left = 0;
 	if (root == NULL)
 		return createTreeNode(data);
-	
+
 
 	if (data < root->data) {
+		root->h++;
 		root->left = insertValueToBST(root->left, data);
 		(root->left)->parent = root;
-		root->h++;
+
 	}
 	else {
+		root->h--;
 		root->right = insertValueToBST(root->right, data);
 		(root->right)->parent = root;
-		root->h--;
+
 	}
-	
+
+	root->h = getH(root);
 
 	if (root->h < 2 && root->h >-2)
 		return root;
-	
+
 
 	// TODO :: chekc balance
 	// balance left three
@@ -59,25 +63,36 @@ TreeNode * insertValueToBST(TreeNode *root, type_data data)
 		temp = root->left;
 		while (temp->right != NULL)
 			temp = temp->right;
-
 		temp->right = root;
-
+		temp->parent = root->parent;
 		root->parent = temp;
 		temp->left = root->left->left;
 		root->left = NULL;
 
-		// update h
-		if (temp->left != NULL)
-			temp->left->h = getH(temp->left);
-		if (temp->right != NULL)
-			temp->right->h = -getH(temp->right);
+
 
 	}
 	else if (root->h < 1) {
-		int x = 10;
-		// balance right three
-
+		temp = root->right;
+		while (temp->left != NULL)
+			temp = temp->left;
+		temp->left = root;
+		temp->parent = root->parent;
+		root->parent = temp;
+		temp->right = root->right->right;
+		root->right = NULL;
 	}
+	// update h
+	temp->h = 0;
+	if (temp->left != NULL) {
+		temp->left->h = getH(temp->left);
+		temp->h = temp->left->h;
+	}
+	if (temp->right != NULL) {
+		temp->right->h = getH(temp->right);
+		temp->h -= temp->right->h;
+	}
+
 
 	//temp->h = getH(temp);
 	return temp;
@@ -102,10 +117,16 @@ void freeBST(TreeNode* root)
 
 int getH(TreeNode *root) {
 	int right = 0, left = 0, result = 0;
-
-	if (root == NULL)
+	TreeNode *temp = root;
+	if (root == NULL || (root->left == NULL && root->right == NULL))
 		return 0;
-	return  getH(root->left) - getH(root->right);
+
+	left = getH(root->left);
+	right = getH(root->right);
+	result = (left - right);
+	/*if (root == NULL)
+		return 0;*/
+	return result;
 
 
 }
